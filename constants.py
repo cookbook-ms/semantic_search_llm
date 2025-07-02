1866d0a48098fae8edca81a32905642650ac03e2
cookbook-ms <m.yang-2@tudelft.nl>
2025-07-02 16:22:25 +0200

    commit

--- constants.py
+++ constants.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 GPT_URL = "https://pd67dqn1bd.execute-api.eu-west-1.amazonaws.com/v1/chat/completions"
 API_URL = "https://pd67dqn1bd.execute-api.eu-west-1.amazonaws.com"
-API_KEY = "sk-JD1R8ZSJ6x-fIoYNyFHosA"
+
 API_ENDPOINT = "/embeddings"
+
